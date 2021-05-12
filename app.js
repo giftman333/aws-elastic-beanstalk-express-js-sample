4f05c374d4cebe92abf29462fddf1ef3b57b71e5
Sam Lee <69924355+giftman333@users.noreply.github.com>
2021-05-12 11:12:39 +0900

    Update app.js
    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello World, Sam!'));
+app.get('/', (req, res) => res.send('Hello World, Sam! Pipeline Test is good.'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
